33a539d43d21cd09157a5f2a11ad69b3ed23e56d
hedinasr <hedi.nasr@sonarsource.com>
2025-03-20 22:57:17 +0100

    BUILD-7756: setup action and integration tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,64 +1,21 @@
-import io
+import json
 import os
 import unittest
 from unittest.mock import MagicMock
-from unittest.mock import mock_open
 from unittest.mock import patch
 
+import pytest
 from github import Repository
+from slack_sdk.errors import SlackApiError
 
+from src.main import create_slack_attachments
+from src.main import get_failed_check_runs
+from src.main import get_github_user_by_id
 from src.main import get_repo
-from src.main import main
+from src.main import get_slack_user_by_name
 
 
 class TestAction(unittest.TestCase):
-    @patch.dict(os.environ, {"GITHUB_EVENT": "check_run",
-                             "GITHUB_ENV": "/tmp/test_env"})
-    @patch("builtins.open", new_callable=mock_open)
-    def test_check_run_event(self, mock_file):
-        main()
-        mock_file.assert_called_once_with("/tmp/test_env", 'a')
-        mock_file().write.assert_called_once_with("event_type=check_run\n")
-
-    @patch.dict(os.environ, {"GITHUB_EVENT": "check_suite",
-                             "GITHUB_CHECK_SUITE_ID": "123",
-                             "GITHUB_TOKEN": "test_token",
-                             "GITHUB_ENV": "/tmp/test_env",
-                             "GITHUB_REPOSITORY": "SonarSource/gh-action_slack-notify"})
-    @patch("sys.stdout", new_callable=io.StringIO)
-    @patch("builtins.open", new_callable=mock_open)
-    @patch("src.main.Github")
-    def test_check_suite_event(self, mock_github, mock_file, mock_stdout):
-        mock_repo = MagicMock()
-        mock_check_suite = MagicMock()
-        mock_check_run_success = MagicMock()
-        mock_check_run_failure = MagicMock()
-        mock_app = MagicMock()
-        mock_app.name = "TestApp"
-
-        mock_check_run_success.conclusion = "success"
-        mock_check_run_failure.conclusion = "failure"
-        mock_check_run_failure.app = mock_app
-        mock_check_run_failure.head_sha = "test_sha"
-
-        mock_check_suite.get_check_runs.return_value = [
-            mock_check_run_success,
-            mock_check_run_failure
-        ]
-        mock_repo.get_check_suite.return_value = mock_check_suite
-        mock_github.return_value.get_repo.return_value = mock_repo
-
-        main()
-
-        self.assertEqual(mock_stdout.getvalue(
-        ), "Retrieving failed check runs for check suite ID: 123\n")
-        self.assertEqual(mock_file().write.call_count, 3)
-        calls = mock_file().write.call_args_list
-        print(calls)
-        self.assertEqual(calls[0][0][0], "app_name=TestApp\n")
-        self.assertEqual(calls[1][0][0], "conclusion=failure\n")
-        self.assertEqual(calls[2][0][0], "head_sha=test_sha\n")
-
     @patch.dict(os.environ, {"GITHUB_TOKEN": "test_token"})
     @patch("src.main.Github")
     def test_get_repo(self, mock_github):
@@ -68,3 +25,57 @@ class TestAction(unittest.TestCase):
         self.assertEqual(repo, mock_repo)
         mock_github.return_value.get_repo.assert_called_once_with(
             "SonarSource/gh-action_slack-notify")
+
+    @patch.dict(os.environ, {"GITHUB_TOKEN": "test_token"})
+    @patch("src.main.Github")
+    def test_get_github_user_by_id(self, mock_github):
+        mock_user = MagicMock()
+        mock_github.return_value.get_user_by_id.return_value = mock_user
+        user = get_github_user_by_id("123")
+        self.assertEqual(user, mock_user)
+        mock_github.return_value.get_user_by_id.assert_called_once_with(123)
+
+    def test_create_slack_attachments(self):
+        check_runs = [MagicMock(), MagicMock()]
+        check_runs[0].conclusion = "failure"
+        check_runs[1].conclusion = "success"
+        attachments = create_slack_attachments(check_runs, "repo", "user")
+        payload = json.loads(attachments)
+        self.assertEqual(len(payload), 2)
+
+    def test_get_failed_check_runs(self):
+        check_suite = MagicMock()
+        check_suite.get_check_runs.return_value = [MagicMock(), MagicMock()]
+        check_suite.get_check_runs()[0].conclusion = "failure"
+        check_suite.get_check_runs()[1].conclusion = "success"
+        failed_check_runs = get_failed_check_runs(check_suite)
+        self.assertEqual(len(failed_check_runs), 1)
+
+    @patch.dict(os.environ, {"SLACK_TOKEN": "test_token"})
+    @patch("src.main.WebClient")
+    def test_get_slack_user_by_name(self, mock_slack):
+        mock_slack.return_value.users_list.return_value = {
+            "members": [{"profile": {"first_name": "John", "last_name": "Doe"}}]}
+        user = get_slack_user_by_name("John", "Doe")
+        self.assertEqual(user, {"profile": {"first_name": "John", "last_name": "Doe"}})
+
+    @patch.dict(os.environ, {"SLACK_TOKEN": "test_token"})
+    @patch("src.main.WebClient")
+    def test_get_slack_user_by_name_not_found(self, mock_slack):
+        mock_slack.return_value.users_list.return_value = {
+            "members": [{"profile": {"first_name": "John", "last_name": "Doe"}}]}
+        user = get_slack_user_by_name("Jane", "Smith")
+        self.assertIsNone(user)
+
+    @patch.dict(os.environ, {"SLACK_TOKEN": "test_token"})
+    @patch("src.main.WebClient")
+    def test_get_slack_user_by_name_api_error(self, mock_slack):
+        mock_slack.return_value.users_list.side_effect = SlackApiError(
+            response=MagicMock(status_code=500, data={"ok": False, "error": "internal_error"}),
+            message="Slack API Error"
+        )
+
+        with pytest.raises(SystemExit) as excinfo:
+            get_slack_user_by_name("Jane", "Smith")
+
+        assert excinfo.value.code == 1
